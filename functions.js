e554773cbdd4e6823876ca1f5e416fb4fdd7efe3
Eto098 <61867713+Eto098@users.noreply.github.com>
2020-12-04 00:09:51 +0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,18 +1,18 @@
 function init(){
+    // :D
     console.log("Hi. Please hire me xD:")
 
-    const logo = document.querySelectorAll('#Logo path');
-    for(let i = 0; i<logo.length; i++){
-        console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-    }
+    // To initialize Gumshoe (scrollspy)
     var spy = new Gumshoe('#my-awesome-nav a', {
         offset: 200
     });
 
+    // To close the gap at the very beginning of page
     var element = document.querySelector("#S1");
     // smooth scroll to element and align it at the center
     element.scrollIntoView({ behavior: 'smooth', block: 'center'});
 
+    // To scroll the page when navbar buttons are clicked
     const NavLinks = document.getElementsByClassName("nav-item");
     for(let i = 0; i<NavLinks.length; i++){
         NavLinks[i].addEventListener("click", () => {
